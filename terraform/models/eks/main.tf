ccb051a4506c194caa018e06f00eef26c87dfd2f
ahmed <aaboelala461@gmail.com>
2025-11-30 01:30:02 +0200

    eks change resource

--- terraform/models/eks/main.tf
+++ terraform/models/eks/main.tf
@@ -104,7 +104,18 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
+
 # EBS CSI Driver IAM Role
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
 resource "aws_iam_role" "ebs_csi" {
   name = "${var.cluster_name}-ebs-csi"
 
@@ -146,7 +157,8 @@ resource "kubernetes_service_account" "ebs_csi-sa" {
 # EBS CSI Driver Addon
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name             = aws_eks_cluster.main.name
-  addon_name               = "aws-ebs-csi"
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.30.0-eksbuild.1"   # مثال — لازم تختار إصدار متوافق
   service_account_role_arn = aws_iam_role.ebs_csi.arn
 }
 
