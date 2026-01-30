40f899300e6d66d483df8ae883149456defd8649
ahmed <aaboelala461@gmail.com>
2026-01-30 03:23:18 +0200

    edit

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,54 +1,54 @@
 variable "region" {
-    description = "The AWS region to deploy the VPC"
-    type        = string
-    default     = "us-east-1" 
-  
+  description = "The AWS region to deploy the VPC"
+  type        = string
+  default     = "us-east-1"
+
 }
 variable "cidr_block" {
-    description = "The CIDR block for the VPC"
-    type        = string
+  description = "The CIDR block for the VPC"
+  type        = string
 }
 
 variable "vpc_name" {
-    description = "The name of the VPC"
-    type        = string
-    default     = "todo-list-vpc"
+  description = "The name of the VPC"
+  type        = string
+  default     = "todo-list-vpc"
 }
 
 variable "vpc_azs" {
-    description = "The availability zones for the VPC"
-    type        = list(string)
+  description = "The availability zones for the VPC"
+  type        = list(string)
 }
 
 variable "vpc_private_subnets" {
-    description = "The private subnets for the VPC"
-    type        = list(string)
+  description = "The private subnets for the VPC"
+  type        = list(string)
 }
 
 variable "vpc_public_subnets" {
-    description = "The public subnets for the VPC"
-    type        = list(string)
+  description = "The public subnets for the VPC"
+  type        = list(string)
 }
 variable "cluster_name" {
-    description = "The name of the EKS cluster"
-    type        = string
-    default     = "my-todo-list-eks-cluster"
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "my-todo-list-eks-cluster"
 }
 //////////////////////////////////////////////////////////////////////////////////////
 variable "cluster_version" {
-    description = "The version of the EKS cluster"
-    type        = string
+  description = "The version of the EKS cluster"
+  type        = string
 }
 
 variable "node_groups" {
-  description   = "EKS node groups configuration"
-  type          = map(object({
+  description = "EKS node groups configuration"
+  type = map(object({
     instance_types = list(string)
     capacity_type  = string
     scaling_config = object({
       desired_size = number
       max_size     = number
-      min_size     = number 
+      min_size     = number
     })
     ssh_key_name = optional(string)
   }))
@@ -57,31 +57,31 @@ variable "node_groups" {
       instance_types = ["t2.medium"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
-        desired_size = 2
-        max_size     = 3
+        desired_size = 3
+        max_size     = 4
         min_size     = 1
       }
-        ssh_key_name = "my-key"
+      ssh_key_name = "my-key"
     }
   }
 }
 
 variable "argo_cd_namespace" {
-    description = "The namespace for ArgoCD"
-    type        = string
-    default     = "argocd-ns"
+  description = "The namespace for ArgoCD"
+  type        = string
+  default     = "argocd-ns"
 }
 
 variable "app_namespace" {
-    description = "The namespace for the Application"
-    type        = string
-    default     = "todo-list-app-ns"
-  
+  description = "The namespace for the Application"
+  type        = string
+  default     = "todo-list-app-ns"
+
 }
 
 variable "observability_namespace" {
-    description = "The namespace for Observability"
-    type        = string
-    default     = "observability-ns"
-  
+  description = "The namespace for Observability"
+  type        = string
+  default     = "observability-ns"
+
 }
